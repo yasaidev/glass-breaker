0edf24ab0efedfd3f41c55f3f59938ce53006725
ryo <3955027+older4@users.noreply.github.com>
2020-12-03 19:08:23 +0900

    [add] 仮のusageを追加

--- src/views/Usage.vue
+++ src/views/Usage.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <h1>使い方のページ</h1>
-    <p>そのうち書く</p>
+  <div id="usage">
+    <h2>使い方</h2>
+    <ol>
+      <li>割れやすそうなワイングラスを用意！！</li>
+      <li>Analysisボタンを押してカウントダウン開始！！！</li>
+      <li>カウントダウン終了後，ワイングラスを弾いて音を鳴らそう！</li>
+      <li>Playボタンを押して破壊音をグラスにぶつけろ！！😎</li>
+    </ol>
   </div>
 </template>
+<style scoped>
+li {
+  /* font-weight: bold; */
+}
+</style>
