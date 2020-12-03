48cf6e67303616a56de04dcc4874934d54267978
ryo <3955027+older4@users.noreply.github.com>
2020-12-03 20:02:35 +0900

    [add] イラストを追加

--- src/views/Usage.vue
+++ src/views/Usage.vue
@@ -3,14 +3,37 @@
     <h2>使い方</h2>
     <ol>
       <li>割れやすそうなワイングラスを用意！！</li>
-      <li>Analysisボタンを押してカウントダウン開始！！！</li>
-      <li>カウントダウン終了後，ワイングラスを弾いて音を鳴らそう！</li>
-      <li>Playボタンを押して破壊音をグラスにぶつけろ！！😎</li>
+      <v-img
+        class="usage_image"
+        src="https://1.bp.blogspot.com/-gEwhPpCeXBE/Xlyf2nv9ueI/AAAAAAABXqE/swxnHxnd0O4sOA1zxJyVf7J9157PxrosACNcBGAsYHQ/s450/job_hotel_roomservice_woman.png"
+        contain
+      />
+      <li>Analysisボタンでカウントダウン開始！！！</li>
+      <v-img
+        class="usage_image"
+        src="https://1.bp.blogspot.com/-4hrDcnSgASQ/V2vYGPWkyhI/AAAAAAAA77k/sWXXaJSBB9MD3r1XM5MWwbrQG87lmEDUQCLcB/s400/jigenbakudan.png"
+        contain
+      />
+      <li>カウントダウン後グラスを弾いて音を鳴らせ！</li>
+      <v-img
+        class="usage_image"
+        src="https://4.bp.blogspot.com/-5olEC5Nc1wY/W64DTkuIVzI/AAAAAAABPFw/5Gh7wSIMIAw4e3RjNxEBZMCPnxMjb7VCACLcBGAs/s800/band_three_man.png"
+        contain
+      />
+      <li>Playボタンで破壊音をグラスにぶつけろ！！😎</li>
+      <v-img
+        class="usage_image"
+        src="https://1.bp.blogspot.com/-4caVesBxeiU/VZt5i8YejlI/AAAAAAAAu8k/PDYOpNEc21Q/s400/tougeika_shippai_waru.png"
+        contain
+      />
     </ol>
   </div>
 </template>
-<style scoped>
-li {
-  /* font-weight: bold; */
+<style lang="scss" scoped>
+.usage_image {
+  @media only screen and (max-device-width: 480px) {
+    max-height: 135px;
+  }
+  max-height: 200px;
 }
 </style>
