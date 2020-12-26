ffc6f74e5259e558135e8f61d9833a537b79738a
ryo <3955027+older4@users.noreply.github.com>
2020-12-26 17:02:46 +0900

    [change] minに切り替えて軽量化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,20 +12,11 @@ import AsyncLoadError from "@/components/AnalysisLoadError.vue";
 export default {
   name: "Home",
   components: {
-    // AudioAnalysis: () => import("@/components/AudioAnalysis.vue"),
     AudioAnalysis: () => ({
-      // The component we want to load.
       component: import("@/components/AudioAnalysis.vue"),
-      // The component to use as a placeholder while the
-      // async component is loading.
       loading: AsyncLoading,
-      // The component to render instead if there is an error
-      // loading the async component.
       error: AsyncLoadError,
-      // The delay before the loading component is shown.
       delay: 100,
-      // If this timeout is reached, the async component is considered
-      // to have failed loading.
       timeout: 100000,
     }),
   },
