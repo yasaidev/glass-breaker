84d5b856965d942f716e31bdcc9f5aa834c18c49
ryo <3955027+older4@users.noreply.github.com>
2021-01-07 18:32:02 +0900

    [add] usageを追加

--- src/views/Usage.vue
+++ src/views/Usage.vue
@@ -1,39 +1,61 @@
 <template>
-  <div id="usage">
-    <h2>使い方</h2>
-    <ol>
-      <li>割れやすそうなワイングラスを用意！！</li>
-      <v-img
-        class="usage_image"
-        src="https://1.bp.blogspot.com/-gEwhPpCeXBE/Xlyf2nv9ueI/AAAAAAABXqE/swxnHxnd0O4sOA1zxJyVf7J9157PxrosACNcBGAsYHQ/s450/job_hotel_roomservice_woman.png"
-        contain
-      />
-      <li>Analysisボタンでカウントダウン開始！！！</li>
-      <v-img
-        class="usage_image"
-        src="https://1.bp.blogspot.com/-4hrDcnSgASQ/V2vYGPWkyhI/AAAAAAAA77k/sWXXaJSBB9MD3r1XM5MWwbrQG87lmEDUQCLcB/s400/jigenbakudan.png"
-        contain
-      />
-      <li>カウントダウン後グラスを弾いて音を鳴らせ！</li>
-      <v-img
-        class="usage_image"
-        src="https://4.bp.blogspot.com/-5olEC5Nc1wY/W64DTkuIVzI/AAAAAAABPFw/5Gh7wSIMIAw4e3RjNxEBZMCPnxMjb7VCACLcBGAs/s800/band_three_man.png"
-        contain
-      />
-      <li>Playボタンで破壊音をグラスにぶつけろ！！😎</li>
-      <v-img
-        class="usage_image"
-        src="https://1.bp.blogspot.com/-4caVesBxeiU/VZt5i8YejlI/AAAAAAAAu8k/PDYOpNEc21Q/s400/tougeika_shippai_waru.png"
-        contain
-      />
-    </ol>
-  </div>
+  <v-container>
+    <v-row justify="center" align="center">
+      <v-col cols="12">
+        <h2>準備</h2>
+      </v-col>
+      <v-col cols="12" md="6">
+        <v-img
+          max-height="200px"
+          max-width="200px"
+          src="../assets/img/Preparation.png"
+          class="mr-md-0 mx-auto"
+        />
+      </v-col>
+      <v-col cols="12" md="6">
+        <ul class="text-md-left">
+          <li>スマホもしくはパソコン</li>
+          <li><span class="highlight">割れやすそうな</span>ワイングラス</li>
+          <li><span class="highlight">いい感じの長さ</span>のストロー</li>
+          <li>できれば<span class="highlight">爆音スピーカー</span></li>
+          (最悪スマホスピーカーでもOK)
+        </ul>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h2>破壊</h2>
+      </v-col>
+      <v-col cols="12">
+        <ol class="text-center">
+          <li>Analysisボタンを押す</li>
+          (マイクの確認ダイアログがでたら許可)
+          <li><span class="highlight">もう一度</span>Analysisボタンを押す</li>
+          <li>
+            <span class="highlight">カウントダウン後</span
+            >グラスをはじいて音を出す
+          </li>
+          <li>サイトが音を自動分析</li>
+          <li>Playボタンは破壊音を再生！</li>
+        </ol>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="12">
+        <h2>ポイント</h2>
+      </v-col>
+      <v-col cols="12">
+        <ul class="text-center">
+          <li><span class="highlight">爆音</span>で破壊音を再生</li>
+          <li>音をグラスに<span class="highlight">近づける</span></li>
+        </ul>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <style lang="scss" scoped>
-.usage_image {
-  @media only screen and (max-device-width: 480px) {
-    max-height: 135px;
-  }
-  max-height: 200px;
+.highlight {
+  background: linear-gradient(transparent 50%, #ffff66 0%);
+  line-height: 1.3em;
 }
 </style>
